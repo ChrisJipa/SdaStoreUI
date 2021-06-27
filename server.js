92e1753dcb1a2071cc6e1315ff9f0f458bcbd960
ChrisJipa <ciprian.jipa78@gmail.com>
2021-06-27 18:55:48 +0300

    corrected server.js file

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
-//Install express server
-const express = require('express');
-const path = require('path');
+//Redirect all traffic from http to https
+function requireHTTPS(req, res, next) {
+  //The 'X-forwarded proto' check is for Heroku
+  if (!req.secure && req.get('X-forwarded-proto') !== 'https') {
+    return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+}
 
+const express = require('express');
+//const path = require('path');
 const app = express();
+app.use(requireHTTPS);
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/angular-app-heroku'));
+app.use(express.static('./dist/sda-store-ui'));
 
-app.get('/*', (req, res) =>
-  res.sendFile('index.html', {root: 'dist/angular-app-heroku/'}),
+app.get('/*', function (req, res, next) {
+    res.sendFile('index.html', {root: 'dist/sda-store-ui/'});
+  }
 );
 
 // Start the app by listening on the default Heroku port
