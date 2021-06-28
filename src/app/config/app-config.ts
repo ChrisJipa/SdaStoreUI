81e87adf6752476db7fc2469c9e1f7a97345de94
ChrisJipa <ciprian.jipa78@gmail.com>
2021-06-28 12:55:53 +0300

    removed trailing slash from the API_PATH in api-config

--- src/app/config/app-config.ts
+++ src/app/config/app-config.ts
@@ -1,4 +1,4 @@
 export class AppConfig {
-  public static API_PATH = ' https://sda-store.herokuapp.com/';
+  public static API_PATH = ' https://sda-store.herokuapp.com';
   public static AUTHORIZATION_HEADER = 'auth_header';
 }
